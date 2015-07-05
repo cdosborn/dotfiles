1ef3cbe9e6aa34a90339edb330a0ef3702441105
Connor Osborn <cdosborn@uw.edu>
2015-07-05 16:58:40 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,8 @@ if [ -d ~/.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &>/dev/null
 fi;
+
+# Install firefox custom theme
+#
+# ln -sf ~/dotfiles/firefox/chrome/ ~/Library/Application\ Support/Firefox/Profiles/*.default/chrome
+#
