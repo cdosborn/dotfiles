6a107b80ecd798be61777ab5fe692b33f0f30c6d
Connor Osborn <cdosborn@uw.edu>
2015-07-22 10:00:43 -0700

    Add Ag to ubuntu/osx

--- install.sh
+++ install.sh
@@ -9,7 +9,15 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &>/dev/null
 mkdir -p ~/.vim/backups ~/.vim/swaps ~/.vim/undo
 
+if [[ `uname -v` =~ Ubuntu ]]; then
+    apt-get -qq install silversearcher-ag
+fi
+
+if [[ `uname -v` =~ Darwin ]]; then
+    brew install ag
+fi
 # Install firefox custom theme
 #
 # ln -sf ~/dotfiles/firefox/chrome/ ~/Library/Application\ Support/Firefox/Profiles/*.default/chrome
 #
+
