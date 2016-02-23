8113b8750a357005b39d14d4119ac2cea82b6380
Connor Osborn <cdosborn@uw.edu>
2016-02-22 20:29:50 -0700

    wow

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ mkdir -p ~/.vim/backups ~/.vim/swaps ~/.vim/undo
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &>/dev/null
 
-[[ `uname -v` =~ Ubuntu ]] &&
-    [[ `whoami` =~ root ]] && 
+if [[ `uname -v` =~ Darwin ]] && [[ `whoami` =~ root ]]; then
     apt-get -qq install silversearcher-ag
+fi
 
 
 if [[ `uname -v` =~ Darwin ]]; then
