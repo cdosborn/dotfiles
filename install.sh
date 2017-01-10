3914474ac8fa45938f6c0d78217c00654812dd0e
Connor Osborn <cdosborn@email.arizona.edu>
2017-01-10 19:02:47 +0000

    ~~~

--- install.sh
+++ install.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+
+# Symlink conf files
 for file in ~/dotfiles/conf/*; do
     tail=${file##*/}
     ln -sf $file ~/.$tail
 done
 
-# Vim config
+# Install vim
 rm -rf ~/.vim;
 ln -sf ~/dotfiles/vim ~/.vim
-# find ~/.vim/ -exec sudo chown -R cdosborn:staff {} +
 mkdir -p ~/.vim/backups ~/.vim/swaps ~/.vim/undo
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &>/dev/null
 
+# Install ubuntu packages
 if [[ `uname -v` =~ Ubuntu ]]; then
-    apt-get -qq install silversearcher-ag
+    ~/dotfiles/ubuntu/install.sh
 fi
 
-
+# Install osx packages
 if [[ `uname -v` =~ Darwin ]]; then
-    # Install ag if it doesnt exist
-     ~/dotfiles/osx/install.sh
+    ~/dotfiles/osx/install.sh
 fi
 
 source ~/dotfiles/conf/bashrc
---
+++ ubuntu/install.sh
@@ -0,0 +1,8 @@
+# Get the latest version of git
+sudo apt-add-repository ppa:git-core/ppa -y
+sudo apt-get update
+sudo apt-get install git
+
+# Install ag
+apt-get -qq install silversearcher-ag
+
