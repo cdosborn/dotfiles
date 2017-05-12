2f4a7459533dd25efc12cdddf448b98ca90aab0a
Connor Osborn <cdosborn@email.arizona.edu>
2017-05-12 17:11:37 +0000

    :0

--- install.sh
+++ install.sh
@@ -14,10 +14,15 @@ done
 
 # Install vim
 rm -rf ~/.vim;
-mkdir -p ~/.vim ~/.vim/backups ~/.vim/swaps ~/.vim/undo ~/.vim/colors
-ln {~/dotfiles/vim,~/.vim}/colors/uncolor.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/b44ea685aa2899316e58cba646095fa7551f8808/plug.vim
+mkdir -p ~/.vim ~/.vim/backups ~/.vim/swaps ~/.vim/undo ~/.vim/colors ~/.vim/autoload
+for file in ~/dotfiles/vim/autoload/*; do
+    name=`basename $file`
+    ln -sf $file ~/.vim/autoload/$name
+done
+for file in ~/dotfiles/vim/colors/*; do
+    name=`basename $file`
+    ln -sf $file ~/.vim/colors/$name
+done
 
 # Install ubuntu packages
 if [[ `uname -v` =~ Ubuntu ]]; then
