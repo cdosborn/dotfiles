0a8b56c1dc3b530165de2ded820eebc952f74df1
Connor Osborn <cdosborn@email.arizona.edu>
2017-09-01 10:38:52 -0700

    ...

--- install.sh
+++ install.sh
@@ -24,6 +24,22 @@ if [[ ! -d ~/dotfiles ]]; then
     git clone https://github.com/cdosborn/dotfiles.git ~/dotfiles
 fi;
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until this script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install ubuntu packages
+if [[ `uname -v` =~ Ubuntu ]]; then
+    ~/dotfiles/ubuntu/apt-install.sh
+fi
+
+# Clone the dotfiles repo if it doesn't exist
+if [[ ! -d ~/dotfiles ]]; then
+    git clone https://github.com/cdosborn/dotfiles.git ~/dotfiles
+fi;
+
 # Symlink conf files
 for file in ~/dotfiles/conf/*; do
     name=`basename $file`
