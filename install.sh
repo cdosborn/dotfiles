68f45cbac64b075945485763fc36cdfa1ceca063
Connor Osborn <cdosborn@email.arizona.edu>
2017-09-05 20:19:17 +0000

    Remove redundant lines

--- install.sh
+++ install.sh
@@ -24,12 +24,6 @@ if [[ ! -d ~/dotfiles ]]; then
     git clone https://github.com/cdosborn/dotfiles.git ~/dotfiles
 fi;
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until this script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Install ubuntu packages
 if [[ `uname -v` =~ Ubuntu ]]; then
     ~/dotfiles/ubuntu/apt-install.sh
