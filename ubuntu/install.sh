523f1a888ee72dd145f31c6b25eef8d8ab8bc03c
Connor Osborn <cdosborn@email.arizona.edu>
2018-02-21 20:04:32 +0000

    WOOO

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
 # Get the latest version of git
-sudo apt-add-repository ppa:git-core/ppa -y
+sudo apt-add-repository -y ppa:git-core/ppa
+sudo apt-add-repository -y ppa:pi-rho/dev
 sudo apt-get update
-sudo apt-get -qy install git xsel silversearcher-ag tree moreutils ctags jq slim xscreensaver entr tmux
+
+for pkg in git xsel silversearcher-ag tree moreutils ctags jq slim xscreensaver entr tmux; do
+    sudo apt-get install -qy "$pkg";
+    if [ "$?" -ne 0 ]; then
+        echo "Note: $pkg wasn't found, and could not be installed." >&2;
+    fi
+done;
